4e08170c0824a56c8a110bb69cdaa29dda7610a4
swatwakathane <swatssk8180@gmail.com>
2024-02-28 18:14:35 +0530

    8th commit

--- Blog/main.js
+++ Blog/main.js
@@ -18,3 +18,27 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+// DASHBOARD TOGGLE ON SMALL DEVICES
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+
+
+const hideSidebar = () => {
+    sidebar.style.left = '-100%';
+    hideSidebarBtn.style.display = 'none';
+    showSidebarBtn.style.display = 'inline-block';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
