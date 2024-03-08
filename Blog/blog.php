1f6862f21d9cb0a4cf1d50fc3321b447eac8ad0f
swatwakathane <swatssk8180@gmail.com>
2024-03-08 15:33:36 +0530

    8th commit

---
+++ Blog/blog.php
@@ -0,0 +1,194 @@
+<?php
+include 'partials/header.php';
+?>
+
+<!-- ----------------------- SEARCH BLOG ---------------------------- -->
+
+    <section class="search__bar">
+        <form action="" class="container search__bar-container">
+            <div>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" name="" placeholder="search">
+            </div>
+            <button type="submit" class="btn">Go</button>
+        </form>
+    </section>
+
+    <!-- ------------------------------------------ POSTS ------------------------------------------- -->
+
+    <section class="posts">
+        <div class="container posts__container">
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+        </div>
+    </section>
+    <section class="category__buttons">
+        <div class="container category__buttons-container">
+            <a href="" class="category__button">Travel</a>
+            <a href="" class="category__button">Arts</a>
+            <a href="" class="category__button">Food</a>
+            <a href="" class="category__button">Wild Life</a>
+            <a href="" class="category__button">Tech</a>
+            <a href="" class="category__button">Music</a>
+        </div>
+    </section>
+
+    
+
+<?php
+include 'partials/footer.php';
+?>
---
+++ Blog/index2.php
@@ -0,0 +1,203 @@
+<?php
+include 'partials/header.php';
+?>
+
+<!-- ----------------------- FEATURED POST ---------------------------- -->
+
+    <section class="featured">
+        <div class="container featured__container">
+            <div class="post__thumbnail">
+                <img src="images/blog1.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Wilf Life</a>
+                <h2 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit. Veniam?</a></h2>
+                <p class="post__body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, saepe. Voluptate laboriosam tempora dolorum minima perspiciatis necessitatibus magni laborum praesentium optio velit, rem iure alias facere laudantium accusamus reprehenderit suscipit.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="images/avatar2.jpg">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Swatwa Kathane</h5>
+                        <small>april 1, 2024 - 14:30</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="posts">
+        <div class="container posts__container">
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.html">Lorem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="images/blog2.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="<?= ROOT_URL?>post.php">Lrem ipsum dolor sit amet consectetur
+                         adipisicing elit. Magnam, molestias?</a></h3>
+                    <p class="post__body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos nisi aspernatur,
+                         a eaque cumque quisquam quia debitis ipsa? Exercitationem suscipit cumque quos?
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="images/avatar3.jpg" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Mills</h5>
+                            <small>April 13, 2024</small>
+                        </div>
+                    </div>
+                
+                
+                
+                </div>
+            </article>
+        </div>
+    </section>
+    <section class="category__buttons">
+        <div class="container category__buttons-container">
+            <a href="" class="category__button">Travel</a>
+            <a href="" class="category__button">Arts</a>
+            <a href="" class="category__button">Food</a>
+            <a href="" class="category__button">Wild Life</a>
+            <a href="" class="category__button">Tech</a>
+            <a href="" class="category__button">Music</a>
+        </div>
+    </section>
+
+<?php
+include 'partials/footer.php';
+?>
