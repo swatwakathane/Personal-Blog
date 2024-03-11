e623936be23340d094b04636be5272df07e97e73
swatwakathane <swatssk8180@gmail.com>
2024-03-11 12:14:01 +0530

    12th commit

--- Blog/config/constants.php
+++ Blog/config/constants.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
+
 define('ROOT_URL', 'http://localhost/ReadyProject/Blog/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'swatwa');
 define('DB_PASS', 'admin1234');
 define('DB_NAME', 'blog');
-session_start();
