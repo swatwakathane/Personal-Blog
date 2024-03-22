e0099e3d49059ac9b1dd9e1946ff09f4d35c17e4
swatwakathane <swatssk8180@gmail.com>
2024-03-22 13:21:42 +0530

    new commit

---
+++ Blog/admin/add-post-logic.php
@@ -0,0 +1,34 @@
+<?php
+require 'config/database.php';
+
+if(isset($_POST['submit'])) {
+    $author_id = $_SESSION['user-id'];
+    $title = filter_var($_POST['title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $body = filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $category_id = filter_var($_POST['category'], FILTER_SANITIZE_NUMBER_INT);
+    $is_featured = filter_var($_POST['is_featured'], FILTER_SANITIZE_NUMBER_INT);
+    $thumbnail = $_FILES['thumbnail'];
+
+    // check for is_featured
+    $is_featured = $is_featured == 1 ? : 0;
+
+    // validate form data
+    if(!$title) {
+        $_SESSION['add-post'] = "Enter valid title for post";
+    } elseif (!$category_id) {
+        $_SESSION['add-post'] = "Select post category";
+    } elseif (!$body) {
+        $_SESSION['add-post'] = "Please write Genuine content";
+    } elseif (!$thumbnail['name']) {
+        $_SESSION['add-post'] = "Please upload thumbnail";
+    } else {
+        // validate thumbnail
+        $time = time();
+        $thumbnail_name = $time . $thumbnail['name'];
+        $thumbnail_tmp_name = $thumbnail['tmp_name'];
+        $thumbnail_destination_path = '../images/' . $thumbnail_name;
+
+        // check for file type
+    }
+
+}
